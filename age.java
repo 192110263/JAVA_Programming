3b9260c66f04885313612b46c01a159ef66b0c5f
madhav <113962975+192110263@users.noreply.github.com>
2024-01-09 10:56:12 +0530

    Delete age.java

--- age.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class age {
-    public static void main(String[] args){
-        System.out.println("enter enter the number");
-        Scanner sc= new Scanner(System.in);
-        int a=sc.nextInt();
-        System.out.print("the entered number is:"+4);
-    }
-}
